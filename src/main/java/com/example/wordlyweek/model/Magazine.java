937f2f3bda278f650a5ceb998f1f72fab3180ea8
someswarraosirikonda <someswarraosirikonda@gmail.com>
2024-03-09 14:09:32 +0000

    solution

---
+++ src/main/java/com/example/wordlyweek/model/Magazine.java
@@ -0,0 +1,67 @@
+/*
+ * You can use the following import statements
+ *
+ * import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+ * 
+ * import javax.persistence.*;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.wordlyweek.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "magazine")
+public class Magazine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int magazineId;
+
+    @Column(name = "title")
+    private String magazineName;
+
+    @Column(name = "publicationdate")
+    private String publicationDate;
+
+    @ManyToMany(mappedBy = "magazines")
+    @JsonIgnoreProperties("magazines")
+    private List<Writer> writers;
+
+    public Magazine() {
+    }
+
+    public int getMagazineId() {
+        return magazineId;
+    }
+
+    public void setMagazineId(int magazineId) {
+        this.magazineId = magazineId;
+    }
+
+    public String getMagazineName() {
+        return magazineName;
+    }
+
+    public void setMagazineName(String magazineName) {
+        this.magazineName = magazineName;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public List<Writer> getWriters() {
+        return writers;
+    }
+}
